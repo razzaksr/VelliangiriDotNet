51e1fcf7d165c3080349c764d241ae93b8a1d2be
razzaksr <razzaksr@gmail.com>
2021-04-23 13:14:37 +0530

    demo c#

---
+++ Fuel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Projects
+{
+    class Fuel
+    {
+        public void hello()
+        {
+            Console.WriteLine(new Random().Next(100));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ namespace Projects
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Classname object=new Constructor()
+            Fuel fuel=new Fuel();
+            fuel.hello();
+            /* string name=Console.ReadLine();
+            Console.WriteLine("Welcome to Zealous Tech Corp Mr./Miss./Mrs."+name);
+            Console.WriteLine("Tell us liters you filled in bike: ");
+            int liter=Convert.ToInt32(Console.ReadLine());//Int32.Parse(Console.ReadLine());
+            int drove=Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Milage: "+(drove/liter)); */
         }
     }
 }
