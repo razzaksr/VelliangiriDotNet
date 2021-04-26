18ee043d92b5b0ae620806482806331117aeffef
razzaksr <razzaksr@gmail.com>
2021-04-26 13:15:11 +0530

    Swap by arithmetic

--- Program.cs
+++ Program.cs
@@ -6,15 +6,29 @@ namespace Projects
     {
         static void Main(string[] args)
         {
-            //Classname object=new Constructor()
-            Fuel fuel=new Fuel();
-            fuel.hello();
-            /* string name=Console.ReadLine();
-            Console.WriteLine("Welcome to Zealous Tech Corp Mr./Miss./Mrs."+name);
-            Console.WriteLine("Tell us liters you filled in bike: ");
-            int liter=Convert.ToInt32(Console.ReadLine());//Int32.Parse(Console.ReadLine());
-            int drove=Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Milage: "+(drove/liter)); */
+            //Int32 _1=90, _2=45, _3=0;
+            //Double _1=3.6,_2=75.09,_3=0.0;
+            char _1='Z',_2='Q',_3='\0';
+            Console.WriteLine(_1+" "+_2);
+
+            // shorthand: _1=_1+_2;>> _1+=_2
+
+            // swap by + -
+            _1+=_2;//_1=_1+_2;
+            _2=(char)(_1-_2);
+            _1-=_2;//_1=_1-_2;
+
+            /* // swap by * /
+            _1*=_2;
+            _2=_1/_2;
+            _1/=_2; */
+
+            /* //swap by third variable
+            _3=_1;
+            _1=_2;
+            _2=_3; */
+
+            Console.WriteLine("After swap: "+_1+" and "+_2);
         }
-    }
+    }   
 }
