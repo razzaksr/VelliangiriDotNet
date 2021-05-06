484cf65bc0ed023488343af7384d18c6cf408c7e
razzaksr <razzaksr@gmail.com>
2021-05-06 10:16:25 +0530

    patterns

--- Program.cs
+++ Program.cs
@@ -6,77 +6,78 @@ namespace Projects
     {
         static void Main(string[] args)
         {
-            int collection=0, seek=0, count=0;
-            Console.WriteLine("Tell us which amount to find count: ");
-            seek=Int32.Parse(Console.ReadLine());
-            for(int street=1;street<=4;street++)
-            {
-                for(int house=1;house<=10;house++)
-                {
-                    Console.WriteLine("Tell us amount to donate: ");
-                    int amt=Int32.Parse(Console.ReadLine());
-                    collection+=amt;
-                    if(amt==seek)
-                    {
-                        count++;
-                    }
-                }
-            }
-            Console.WriteLine("Total collection for covid relief fund: "+collection);
-            Console.WriteLine(count+" of person gave "+seek+" amount");
-            /* int collection=0, max1=Int32.MinValue, max2=Int32.MinValue,max3=Int32.MinValue;
-            for(int street=15;street<=18;street++)
-            {
-                for(int house=1;house<=10;house++)
-                {
-                    Console.WriteLine("Request to donate for relief fund for covid");
-                    int amt=Int32.Parse(Console.ReadLine());
-                    collection+=amt;
-                    if(amt>max1)
-                    {
-                        max3=max2;
-                        max2=max1;
-                        max1=amt;
-                    }
-                    else if(amt>max2&&amt!=max1)
-                    {
-                        max3=max2;
-                        max2=amt;
-                    }
-                    else if(amt>max3&&amt!=max1&&amt!=max2)
-                    {
-                        max3=amt;
-                    }
-                }
-            }
-            Console.WriteLine("Total collection for covid relief fund: "+collection);
-            Console.WriteLine(max1+","+max2+","+max3+" is Top 3 maximum donations obtained"); */
-            /* String chart="-------\n";
+            // left upper floyd
             for(int row=1;row<=5;row++)
             {
-                chart+="|";
-                for(int seats=1;seats<=4;seats++)
+                for(int col=1;col<=row;col++)
                 {
-                    Console.WriteLine("Bring the amount: ");
-                    int amt=Int32.Parse(Console.ReadLine());
-                    if(seats==3)
-                    {
-                        chart+=" ";
-                    }
-                    if(amt>=260)
-                    {
-                        Console.WriteLine("You can travel @ "+seats+" in "+row+" row");
-                        chart+="$";
-                    }
-                    else{
-                        Console.WriteLine("Insufficient to travel");
-                        chart+="#";
-                    }
+                    Console.Write("$");
                 }
-                chart+="|\n";
+                Console.WriteLine();
             }
-            chart+="-------\n";
-            Console.WriteLine(chart); */
+            // right upper floyd
+            for(int row=1;row<=5;row++)
+            {
+                for(int spc=5-1;spc>=row;spc--)
+                {
+                    Console.Write(" ");
+                }
+                for(int col=1;col<=row;col++)
+                {
+                    Console.Write("$");
+                }
+                Console.WriteLine();
+            }
+            // upper pascal
+            for(int row=1;row<=5;row++)
+            {
+                for(int spc=5-1;spc>=row;spc--)
+                {
+                    Console.Write(" ");
+                }
+                for(int col=1;col<=row;col++)
+                {
+                    Console.Write("$ ");
+                }
+                Console.WriteLine();
+            }
+            // upper pyramid
+            int limit=1;
+            for(int row=1;row<=5;row++)
+            {
+                for(int spc=5-1;spc>=row;spc--)
+                {
+                    Console.Write(" ");
+                }
+                for(int col=1;col<=limit;col++)
+                {
+                    Console.Write("$");
+                }
+                limit+=2;Console.WriteLine();
+            }
+            // Patterns
+            /* for(int row=1;row<=5;row++)
+            {
+                for(int col=1;col<=5;col++)
+                {
+                    Console.Write("$");
+                }
+                Console.WriteLine();
+            }
+
+            String alpha="Mohamed";
+            /* for(int row=1;row<alpha.Length;row++)
+            {
+                Console.WriteLine(alpha);
+            } 
+            for(int row=1;row<=alpha.Length;row++)
+            {
+                for(int col=0;col<alpha.Length;col++)
+                {
+                    Console.Write((int)alpha[col]+" ");
+                }
+                Console.WriteLine();
+            } */
         }
     }   
 }
