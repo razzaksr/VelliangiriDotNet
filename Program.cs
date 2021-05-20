266425d2e5ee37a1fc36b2deeeecd0738e08b4aa
razzaksr <razzaksr@gmail.com>
2021-05-20 09:36:55 +0530

    Recursive

--- Program.cs
+++ Program.cs
@@ -4,42 +4,55 @@ namespace Projects
 {
     class Program
     {
-        
-        //modifier return_type fun_name(parameters)
-        public static void hello()
+        public static String[] filter(String[] yet)
         {
-            Console.WriteLine("Bhoom you have called your first function");
+            String[] fname=new String[yet.Length];
+            for(int index=0;index<yet.Length;index++)
+            {
+                fname[index]=yet[index].Split(" ")[1];
+            }
+            return fname;
         }
 
-        public static void alter(double yet)// call by value
+        public static int clickOnIt()
         {
-            Console.WriteLine("What i get: "+yet);
-            yet-=(yet*0.100);
-            Console.WriteLine("Confirm in alter: "+yet);
+            Random random=new Random();
+            return random.Next(999999);
         }
 
-        public static void change(double[] yet)// call by refernce
+        public static void haiThere(int count=1) // recursive
         {
-            Console.WriteLine("What i get in 2nd index: "+yet[2]);
-            yet[2]+=(yet[2]*0.100);
-            Console.WriteLine("Confirm in alter in 2nd index: "+yet[2]);
+            Console.WriteLine("Hai Called @ "+count);
+            if(count<10)
+            {
+                count++;haiThere(count);
+            }
+            else
+                return;
         }
-        
+
+        public static void list(String[] hai,int index=0)
+        {
+            if(index>=hai.Length)
+                return;
+            Console.Write(hai[index]+" ");
+            index++;
+            list(hai,index);
+        }
+
         static void Main(string[] react)
         {
-            //Program.hello();
-            double[] wind={10.5,7.2,12.6,0.08,4.1,7.2,8.8};
-
-            Program.change(wind);
-
-            Console.WriteLine("After change called 2nd index in main: "+wind[2]);
-
-            double hai=9.2;
-            Program.alter(2.3);
-            Program.alter(hai);
-            Console.WriteLine("Hai in Main: "+hai);
-            Program.alter(wind[0]);
-            Console.WriteLine(wind[0]+" index value in main");
+            /* String[] cast={"Tom Holland","Chris Pratt","Robert Downey","Chris Evans","Chris Helmsworth"};
+            String[] hai=Program.filter(cast);
+            foreach(String tmp in hai)
+            {
+                Console.WriteLine(tmp);
+            } */
+            /* int otp=Program.clickOnIt();
+            Console.WriteLine("OTP is:"+otp); */
+            //haiThere();
+            String[] cast={"Tom Holland","Chris Pratt","Robert Downey","Chris Evans","Chris Helmsworth"};
+            Program.list(cast);
         }
     }   
 }
