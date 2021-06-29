d2fbb30b4503a66a09ffac25c425c8a6c6bd64ca
razzaksr <47131600+razzaksr@users.noreply.github.com>
2021-06-29 11:31:43 +0530

    files intro

---
+++ Files/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DemoFileStream.write();
+            //DemoFileStream.read();
+
+            //DemoStreams.write();
+            DemoStreams.read();
+        }
+    }
+}
