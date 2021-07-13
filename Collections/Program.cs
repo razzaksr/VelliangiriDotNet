f8d121440fec4b0a6a456daeac0d68423e7dfd33
razzaksr <47131600+razzaksr@users.noreply.github.com>
2021-07-13 11:30:57 +0530

    stack queue arraylist

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -8,63 +8,92 @@ namespace Collections
     {
         static void Main(string[] args)
         {
-            
-            LinkedList<Int32> link=new LinkedList<Int32>();
-            link.AddLast(90);link.AddLast(120);link.AddLast(90);
-            link.AddFirst(500);
 
-            foreach (var item in link)
+            // Queue
+            Queue<Double> line=new Queue<Double>(2);
+            line.Enqueue(9.2);line.Enqueue(68.3);line.Enqueue(11.4);
+            line.Enqueue(79.3);line.Enqueue(45.23);line.Enqueue(68.54);
+            line.Enqueue(132.5);line.Enqueue(0.45);line.Enqueue(99031.1);
+            line.Enqueue(1.345);line.Enqueue(9.123);line.Enqueue(0.3432);
+
+            Console.WriteLine(line.Peek());
+
+            line.Dequeue();line.Dequeue();line.Dequeue();line.Dequeue();
+
+            foreach (var item in line)
             {
                 Console.WriteLine(item);
             }
 
-            Console.WriteLine(link.Last.Value);
+            Console.WriteLine(line.Count);
 
-            link.RemoveFirst();
-            link.RemoveLast();
 
-            IEnumerator enumerator=link.GetEnumerator();
+            //line.TrimExcess();
 
-            Console.WriteLine("Iterate via enum");
-            while(enumerator.MoveNext())
-            {
-                Console.WriteLine(enumerator.Current);
-            }
-
-            link.Remove(120);
-
-            Console.WriteLine(link.Contains(120));
-
+            Console.WriteLine(line.Count);
+            
             /* 
-            List<Object> list=new List<Object>();
-
-            list.Add(12);list.Add("Zealous");
-            list.Add(false);list.Add(8.9);
-            list.Add('R');
-
-            Console.WriteLine("Object @ 3rd index: "+(list[3]));
-
-            list.RemoveAt(4);
-
+            //Stack
+            Stack<String> yet=new Stack<string>();
+            yet.Push("React");yet.Push("Angular");
+            yet.Push("Node");yet.Push("Express");
             
-            list.Insert(1,"Razak Mohamed");
+            //String hai=yet.ToString();
 
-            Console.WriteLine("Object @ 1st: "+list[1]);
-            
-            Console.WriteLine(list.Contains("Zealous"));
+            //Console.WriteLine(hai);
 
-            Console.WriteLine(list.Count);
+            Console.WriteLine(yet.Peek());
 
-            //list.Sort();
+            Console.WriteLine(yet.Pop());
 
+            yet.Push("Java");yet.Push("SpringBoot");
+            yet.Push("JPA");yet.Push("REST");
 
-            foreach (Object item in list)
+            Console.WriteLine(yet.Count);
+
+            yet.Clear();
+
+            String[] hai=yet.ToArray();
+
+            foreach (var item in hai)
             {
                 Console.WriteLine(item);
-            } */
+            }
+ */
+            /*
+            // ArrayList
+            ArrayList obj=new ArrayList();
+            obj.Add("Razak Mohamed");obj.Add("Meharaj");
+            obj.Add("SAbari");obj.Add("Manikandan");
+            obj.Add("Senthil");obj.Add("Balaji");
 
-            
+            ArrayList str=new ArrayList();
+            str.AddRange(obj);
 
+            str.Insert(2,"Nathan");
+            str.InsertRange(3,yet);
+
+            foreach (var item in str)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Angular resides @ "+(str.IndexOf("Angular")));
+
+            str.RemoveRange(6,4);
+
+            Console.WriteLine(str.Contains("SAbari"));
+
+            Console.WriteLine(obj.Contains("SAbari"));
+
+            str.Sort();
+            str.Reverse();
+
+            foreach (var item in str)
+            {
+                Console.WriteLine(item);
+            }
+            */
         }
     }
 }
