3df8da4b8196199b83b72bfc8701e4a6d8fb323e
razzaksr <47131600+razzaksr@users.noreply.github.com>
2021-07-14 11:09:23 +0530

    Collections are over

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -9,91 +9,60 @@ namespace Collections
         static void Main(string[] args)
         {
 
-            // Queue
-            Queue<Double> line=new Queue<Double>(2);
-            line.Enqueue(9.2);line.Enqueue(68.3);line.Enqueue(11.4);
-            line.Enqueue(79.3);line.Enqueue(45.23);line.Enqueue(68.54);
-            line.Enqueue(132.5);line.Enqueue(0.45);line.Enqueue(99031.1);
-            line.Enqueue(1.345);line.Enqueue(9.123);line.Enqueue(0.3432);
+            //SortedList
+            SortedList<long,String> list=new SortedList<long, string>();
+            list.Add(9876567887612,"Razak Mohamed");list.Add(8765567834,"Sabarinathan");
+            list.Add(111111111111,"Rajiya Mohamed");list.Add(876556734345,"Manikandan");
+            list.Add(774586544556,"Balaji");list.Add(12345687654,"Mahesh");
 
-            Console.WriteLine(line.Peek());
+            Console.WriteLine(list.IndexOfKey(111111111111));
 
-            line.Dequeue();line.Dequeue();line.Dequeue();line.Dequeue();
+            Console.WriteLine(list.IndexOfValue("Razak Mohamed"));
 
-            foreach (var item in line)
+            foreach (var item in list)
             {
                 Console.WriteLine(item);
             }
 
-            Console.WriteLine(line.Count);
-
-
-            //line.TrimExcess();
-
-            Console.WriteLine(line.Count);
-            
             /* 
-            //Stack
-            Stack<String> yet=new Stack<string>();
-            yet.Push("React");yet.Push("Angular");
-            yet.Push("Node");yet.Push("Express");
-            
-            //String hai=yet.ToString();
+            // Dictionary
+            Dictionary<String,Int64> contact=new Dictionary<string, long>();
+            contact.Add("Zealous",9597334782);contact.Add("Sheik",987656787644);
+            contact.Add("Manikandan",7656789876);contact.Add("Academy",9597334782);
+            contact.Add("Zealous Tech Corp",1111111111);contact.Add("Sabari",9876567834);
 
-            //Console.WriteLine(hai);
+            contact.Remove("Zealous");
 
-            Console.WriteLine(yet.Peek());
-
-            Console.WriteLine(yet.Pop());
-
-            yet.Push("Java");yet.Push("SpringBoot");
-            yet.Push("JPA");yet.Push("REST");
-
-            Console.WriteLine(yet.Count);
-
-            yet.Clear();
-
-            String[] hai=yet.ToArray();
-
-            foreach (var item in hai)
+            foreach (var item in contact)
             {
                 Console.WriteLine(item);
-            }
- */
-            /*
-            // ArrayList
-            ArrayList obj=new ArrayList();
-            obj.Add("Razak Mohamed");obj.Add("Meharaj");
-            obj.Add("SAbari");obj.Add("Manikandan");
-            obj.Add("Senthil");obj.Add("Balaji");
+            } */
 
-            ArrayList str=new ArrayList();
-            str.AddRange(obj);
+            /* 
+            // Hashtable
+            Hashtable table=new Hashtable();
+            table.Add("Java",8000);table.Add("ASP",12000);
+            table.Add("PHP",12000);table.Add("C#",8000);
+            table.Add("DJango",16000);table.Add("Python",9000);
 
-            str.Insert(2,"Nathan");
-            str.InsertRange(3,yet);
+            Console.WriteLine(table.ContainsKey("SpringBoot"));
+            Console.WriteLine(table.ContainsValue(12000));
 
-            foreach (var item in str)
+            table.Remove("PHP");
+
+            Console.WriteLine(table.ContainsValue(12000));
+
+            ICollection items=table.Keys;
+            foreach (var item in items)
             {
-                Console.WriteLine(item);
+                Console.WriteLine("Key is "+item);
             }
 
-            Console.WriteLine("Angular resides @ "+(str.IndexOf("Angular")));
-
-            str.RemoveRange(6,4);
-
-            Console.WriteLine(str.Contains("SAbari"));
-
-            Console.WriteLine(obj.Contains("SAbari"));
-
-            str.Sort();
-            str.Reverse();
-
-            foreach (var item in str)
+            items=table.Values;
+            foreach (var item in items)
             {
-                Console.WriteLine(item);
-            }
-            */
+                Console.WriteLine("Value is "+item);
+            } */
         }
     }
 }
