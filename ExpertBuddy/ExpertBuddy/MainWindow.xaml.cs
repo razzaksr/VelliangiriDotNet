e6dd2186c904969dff8e6bbdda96d11c96f696b8
razzaksr <47131600+razzaksr@users.noreply.github.com>
2021-07-22 11:56:29 +0530

    Add files via upload

---
+++ ExpertBuddy/ExpertBuddy/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExpertBuddy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            String user = text1.Text;
+            String pas = pass.Password;
+            if (user.Equals("velliangiri") && pas.Equals("salem"))
+            {
+                MessageBox.Show("USer login sucess");
+                text1.Text = "";
+                pass.Password = "";
+            }
+            else
+            {
+                MessageBox.Show("Login failed");
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            text1.Text = "";
+            pass.Password = "";
+        }
+    }
+}
---
+++ Register.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Data;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace ExpertBuddy
+{
+    /// <summary>
+    /// Interaction logic for Register.xaml
+    /// </summary>
+    public partial class Register : Window
+    {
+        SqlConnection connection;
+        SqlCommand cmd;
+        public Register()
+        {
+            connection = new SqlConnection("Data Source=SRDB\\SQLEXPRESS;Initial Catalog=velliangiri;Integrated Security=True");
+            connection.Open();
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            IList<Object> sks = (IList<Object>) skill.SelectedItems;
+            String tmp = "";
+            foreach(Object h in sks)
+            {
+                tmp += h + ",";
+            }
+            cmd = new SqlCommand("insert into expert(name,skill,exp) values(@a,@b,@c)", connection);
+            cmd.Parameters.AddWithValue("@a",name.Text);
+            cmd.Parameters.AddWithValue("@b", tmp);
+            cmd.Parameters.AddWithValue("@c",Int32.Parse(exp.Text));
+
+            cmd.ExecuteNonQuery();
+
+            MessageBox.Show("Expert registered successfully");
+
+            connection.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            name.Text = "";
+            exp.Text="";
+        }
+    }
+}
